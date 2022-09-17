7c592dbd64deee8f3eae407bf5ec66ec229b6cbc
Dave Bage <david_bage@hotmail.com>
2022-09-17 12:50:40 +0100

    Refactored parse code to extract OperatorExists function

--- APD_BooleanCalculator/BooleanCalculator.cs
+++ APD_BooleanCalculator/BooleanCalculator.cs
@@ -36,21 +36,21 @@ namespace APD_BooleanCalculator
 
             if (bool.TryParse(booleanValue, out result)) return result;
 
-            if (booleanValue.Split(" OR ", 2, StringSplitOptions.TrimEntries).Length > 1)
+            if (OperatorExists("OR", booleanValue))
             {
                 var args = ProcessTwoArgumentOperator(booleanValue, "OR");
 
                 return Parse(args[0]) || Parse(args[1]);
             }
 
-            if (booleanValue.Split(" AND ", 2, StringSplitOptions.TrimEntries).Length > 1)
+            if (OperatorExists("AND", booleanValue))
             {
                 var args = ProcessTwoArgumentOperator(booleanValue, "AND");
 
                 return Parse(args[0]) && Parse(args[1]);
             }
 
-            if (booleanValue.Split("NOT", 2, StringSplitOptions.TrimEntries).Length > 1)
+            if (OperatorExists("NOT", booleanValue))
             {
                 return !Parse(booleanValue.Split("NOT", 2, StringSplitOptions.TrimEntries).Last());
             }
@@ -58,12 +58,17 @@ namespace APD_BooleanCalculator
             return result;
         }
 
+        private static bool OperatorExists(string operatorName, string booleanValue)
+        {
+            return booleanValue.Split($"{operatorName}", 2, StringSplitOptions.TrimEntries).Length > 1;
+        }
+
         private static List<string> ProcessTwoArgumentOperator(string booleanValue, string operatorName)
         {
             List<string> arguments = new List<string>();
 
-            arguments.Add(booleanValue.Split($" {operatorName} ", 2, StringSplitOptions.TrimEntries)[0]);
-            arguments.Add(booleanValue.Split($" {operatorName} ", 2, StringSplitOptions.TrimEntries)[1]);
+            arguments.Add(booleanValue.Split($"{operatorName}", 2, StringSplitOptions.TrimEntries)[0]);
+            arguments.Add(booleanValue.Split($"{operatorName}", 2, StringSplitOptions.TrimEntries)[1]);
 
             return arguments;
         }
